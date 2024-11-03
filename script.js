8bbab898004c9dcc8d7e002174da4e87f2d51779
Rojer Hein <164485492+HeinRojer@users.noreply.github.com>
2024-11-03 20:18:07 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function home(){    
+    alert("Home Page");
+    window.location.href="/index.html"
+}
+function info(){
+    alert("Info Page")
+    window.location.href="/info.html"
+}
+function project(){
+    alert("Project Page")
+    window.location.href="/projects.html"
+}
+function about(){
+    alert("About Page")
+    window.location.href="/about.html"
+}
+function contact(){
+    alert("Contact Page")
+    window.location.href="/contact.html"
+}
+function launch(){
+    alert("Launch Page")
+    window.location.href="/launch.html"
+}
+
+
+
